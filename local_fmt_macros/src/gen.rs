ef14891a5cbb0cbe89fbfabf74a0606e6e30d366
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-12-09 22:30:20 +0900

    feat: remove error point from definition ExprCall

--- local_fmt_macros/src/gen.rs
+++ local_fmt_macros/src/gen.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use proc_macro2::{Span, TokenStream};
-use quote::quote;
+use quote::{quote, ToTokens};
 use toml::Table;
 
 use crate::args::Args;
@@ -37,16 +37,45 @@ pub(crate) fn gen_code(path: PathBuf, args: Args) -> syn::Result<TokenStream> {
         global,
     } = args.clone();
 
-    let init: syn::ExprMethodCall = {
-        let def: syn::Expr = unwrap_err_str!(
-            syn::parse_str("local_fmt::LocalFmt::new()"),
-            "definition error please report issue"
-        );
-        let syn::Expr::MethodCall(mut call) = def else {
-            return Err(syn::Error::new(
-                Span::call_site(),
-                "definition error please report issue",
-            ));
+    // local_fmt::LocalFmt<Lang, Key>::new()
+    let init: syn::ExprCall = {
+        let mut call = syn::ExprCall {
+            attrs: Vec::new(),
+            func: Box::new(syn::Expr::Path(syn::ExprPath {
+                attrs: Vec::new(),
+                qself: None,
+                path: {
+                    let mut p = syn::Path::from(syn::Ident::new("local_fmt", Span::call_site()));
+                    p.segments
+                        .push(syn::Ident::new("LocalFmt", Span::call_site()).into());
+                    let new = {
+                        let mut new: syn::PathSegment =
+                            syn::Ident::new("new", Span::call_site()).into();
+                        new.arguments = syn::PathArguments::AngleBracketed(
+                            syn::AngleBracketedGenericArguments {
+                                colon2_token: Some(syn::Token![::](Span::call_site())),
+                                lt_token: syn::Token![<](Span::call_site()),
+                                args: {
+                                    let mut args = syn::punctuated::Punctuated::new();
+                                    args.push(syn::GenericArgument::Type(syn::Type::Verbatim(
+                                        lang.to_token_stream(),
+                                    )));
+                                    args.push(syn::GenericArgument::Type(syn::Type::Verbatim(
+                                        key.to_token_stream(),
+                                    )));
+                                    args
+                                },
+                                gt_token: syn::Token![>](Span::call_site()),
+                            },
+                        );
+                        new
+                    };
+                    p.segments.push(new);
+                    p
+                },
+            })),
+            paren_token: syn::token::Paren(Span::call_site()),
+            args: Default::default(),
         };
         call.args.push(unwrap_err_str!(
             syn::parse_str("Default::default()"),
@@ -83,7 +112,7 @@ pub(crate) fn gen_code(path: PathBuf, args: Args) -> syn::Result<TokenStream> {
 
 fn gen_code_of_app(table: toml::Table, args: Args) -> syn::Result<TokenStream> {
     let Args {
-        locales_path,
+        locales_path: _,
         vis,
         ident,
         lang,
