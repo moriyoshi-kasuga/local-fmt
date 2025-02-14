bc61a3e592f51186b74243eb2d69e3b2f6fe63ea
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-14 22:38:44 +0900

    test: add test

--- local_fmt/tests/gen_const_message.rs
+++ local_fmt/tests/gen_const_message.rs
@@ -1,6 +1,17 @@
 use local_fmt::{gen_const_message, ConstMessage};
 
 #[test]
-fn test_1() {
+fn test_unchecked() {
+    let _: ConstMessage<1> = unsafe { gen_const_message!(unchecked "Hello! ", { 0 }, { 99 }) };
+}
+
+#[test]
+fn test_1_success() {
     let _: ConstMessage<1> = gen_const_message!("Hello! ", { 0 });
+}
+
+#[test]
+#[should_panic]
+fn test_2_failed() {
+    let _: ConstMessage<2> = gen_const_message!("Hello! ", { 0 }, " World!");
 }
