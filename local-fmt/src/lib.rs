5519b7d88ed261439a9e63cd742b7cc540ab3615
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-15 12:29:15 +0900

    feat: underscore to hyphen and add internal crate

--- local-fmt/src/lib.rs
+++ local-fmt/src/lib.rs
@@ -1,11 +1,10 @@
+pub use local_fmt_internal::*;
+
 use std::collections::HashMap;
 use std::hash::Hash;
 
 #[cfg(feature = "macros")]
 pub use local_fmt_macros as macros;
-
-pub mod message;
-pub use message::*;
 
 pub struct LocalFmt<L, M> {
     messages: HashMap<L, M>,
