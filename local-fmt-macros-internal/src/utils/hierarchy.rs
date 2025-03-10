c9bda0b31e06467fd7232c6d3e8237fa539b44b1
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-03-10 13:35:15 +0900

    docs: add doc to Hierarchy struct

--- local-fmt-macros-internal/src/utils/hierarchy.rs
+++ local-fmt-macros-internal/src/utils/hierarchy.rs
@@ -1,3 +1,17 @@
+/// A simple stack-like structure that can be used to keep track of a hierarchy.
+/// This is useful for keeping track of a hierarchy of items, such as a hierarchy of keys.
+/// # Example
+/// ```rust
+/// use local_fmt_macros_internal::utils::hierarchy::Hierarchy;
+/// let mut hierarchy = Hierarchy::new();
+/// hierarchy.process("a", |h| {
+///     h.process("b", |h| {
+///         assert_eq!(h.join("key"), "a.b.key");
+///    });
+///    assert_eq!(h.join("key"), "a.key");
+/// });
+///
+/// assert_eq!(hierarchy.join("key"), "key");
 pub struct Hierarchy<I> {
     items: Vec<I>,
 }
@@ -13,6 +27,9 @@ impl<I> Hierarchy<I> {
         Hierarchy { items: Vec::new() }
     }
 
+    /// Process an item and a function that takes a mutable reference to the hierarchy.
+    /// The item is pushed onto the hierarchy before the function is called and popped off after the function is called.
+    /// Example usage of [Hierarchy]
     pub fn process<T>(&mut self, item: I, func: impl FnOnce(&mut Self) -> T) -> T {
         self.items.push(item);
         let t = func(self);
@@ -26,6 +43,11 @@ impl<I> Hierarchy<I> {
 }
 
 impl<I: AsRef<str>> Hierarchy<I> {
+    /// Join the items in the hierarchy with the current item.
+    /// The items are joined with a period.
+    /// The generic type must implement [AsRef<str>].
+    ///
+    /// Example usage of [Hierarchy]
     pub fn join(&self, current: &str) -> String {
         let mut items = self.items.iter().map(|i| i.as_ref()).collect::<Vec<_>>();
         items.push(current);
@@ -34,6 +56,11 @@ impl<I: AsRef<str>> Hierarchy<I> {
 }
 
 impl<I: ToString> Hierarchy<I> {
+    /// Join the items in the hierarchy with the current item.
+    /// The items are joined with a period.
+    /// The generic type must implement [ToString].
+    ///
+    /// Similarity Example usage of [Hierarchy]
     pub fn joining(&self, current: String) -> String {
         let mut items = self.items.iter().map(|i| i.to_string()).collect::<Vec<_>>();
         items.push(current);
