97f30347a19c8d5930ae9aa3071d7676dcda9875
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-03-10 11:14:27 +0900

    feat: support multiple litstr

--- local-fmt-macros-internal/src/util_macro/arg.rs
+++ local-fmt-macros-internal/src/util_macro/arg.rs
@@ -1,26 +1,30 @@
 use std::str::FromStr;
 
 use proc_macro2::TokenStream;
-use syn::{parse::Parse, LitStr};
+use syn::{parse::Parse, punctuated::Punctuated, spanned::Spanned, LitStr};
 
 use crate::parse::MessageToken;
 
 pub struct Args {
-    pub text: LitStr,
+    pub texts: Punctuated<LitStr, syn::Token![,]>,
 }
 
 impl Parse for Args {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         Ok(Args {
-            text: input.parse()?,
+            texts: Punctuated::parse_terminated(input)?,
         })
     }
 }
 
 impl Args {
     pub fn to_token(&self, is_static: bool) -> syn::Result<TokenStream> {
-        let token = MessageToken::from_str(&self.text.value())
-            .map_err(|v| syn::Error::new(self.text.span(), v))?;
+        let text = self.texts.iter().fold(String::new(), |mut acc, lit| {
+            acc.push_str(&lit.value());
+            acc
+        });
+        let token =
+            MessageToken::from_str(&text).map_err(|v| syn::Error::new(self.texts.span(), v))?;
 
         if is_static {
             Ok(token.to_static_token_stream())
